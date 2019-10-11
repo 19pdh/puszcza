293388bf6c338fb5a2bc2d05ff526750f28bb4bc
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-10-11 20:25:43 +0200

    Add attribute parsing

--- api/api.js
+++ api/api.js
@@ -1,6 +1,14 @@
 const fs = require('fs')
+const frontmatter = require('front-matter')
 
 const POSTS_PATH = './kronika/wpisy'
+
+function getPostAttributes(filePath) {
+  const fileContent = fs.readFileSync(filePath, "utf-8")
+
+  const post = frontmatter(fileContent);
+  return post.attributes
+}
 
 function getPosts() {
   const routesArray = []
@@ -14,13 +22,17 @@ function getPosts() {
           const files = fs.readdirSync(`${POSTS_PATH}/${year}/${month}/${day}`)
           files.forEach((file) => {
             const title = file.substr(0, file.lastIndexOf('.'))
-            const route = `/kronika/${year}/${month}/${day}/${name}`
+            const route = `/kronika/${year}/${month}/${day}/${title}`
             const fsRoute = `${POSTS_PATH}/${year}/${month}/${day}/${file}`
+
+            const attributes = getPostAttributes(fsRoute)
+
             routesArray.push({
               year,
               month,
               day,
               title,
+              attributes,
               file,
               fsRoute,
               route
@@ -39,8 +51,6 @@ function createRoutesArray() {
   let posts = getPosts()
   return posts.map(post => post.route)
 }
-
-createRoutesArray()
 
 module.exports = {
   getPosts,
--- scripts/generateApi.js
+++ scripts/generateApi.js
@@ -1,13 +1,17 @@
+const fs = require('fs')
+
 let { getPosts } = require('../api/api')
 
 let posts = getPosts()
 
-posts = posts.map(({ year, month, day, title, route }) => {
+posts = posts.map(({ year, month, day, title, attributes, route }) => {
     return {
         date: `${year}-${month}-${day}`,
         title,
+        attributes,
         route
     }
 })
 
+fs.writeFile('./dist/api/posts.json', JSON.stringify(posts), (err) => err ? console.log(err) : null)
 console.log(posts)

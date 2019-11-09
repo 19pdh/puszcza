6df360ef621cf5e3e8964fa9795bd0b2ae6aafae
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-11-09 08:33:49 +0100

    Add scale on hover

--- components/PostLink.vue
+++ components/PostLink.vue
@@ -44,6 +44,12 @@ export default {
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
   background: #ffffff;
   text-align: left;
+  transition: transform 300ms ease-in-out;
+}
+
+.post-link:hover {
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  transform: scale(1.02);
 }
 
 .post-link > a {

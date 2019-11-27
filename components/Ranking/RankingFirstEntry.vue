a12cffe8d8a67658df93eec8286485247e5d7bff
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-11-28 00:30:22 +0100

    Add ranking list

---
+++ components/Ranking/RankingFirstEntry.vue
@@ -0,0 +1,49 @@
+<template>
+  <li class="ranking-first-entry">
+    <img class="crown" src="/assets/crown.svg" />
+    <div class="ranking-first-entry__troop">{{ troop }}</div>
+    <div class="ranking-first-entry__points">{{ points }}</div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'RankingFirstEntry',
+  props: {
+    troop: {
+      type: String,
+      required: true
+    },
+    points: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ranking-first-entry {
+  list-style: none;
+  padding: 10px 20px;
+  display: grid;
+
+  width: 150%;
+  grid-template-columns: 15% 60% 25%;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+
+  font-size: 26px;
+}
+
+.crown {
+  margin: auto;
+}
+
+.ranking-first-entry__troop {
+  grid-column: 2;
+}
+
+.ranking-first-entry__points {
+  grid-column: 3;
+}
+</style>
---
+++ pages/punktacja.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="max-width: 900px">
+    <h1>Punktacja</h1>
+    <ranking-list v-if="scores" :scores="scores" />
+    <p v-else>Loading...</p>
+  </div>
+</template>
+
+<script>
+import RankingList from '~/components/Ranking/RankingList'
+
+export default {
+  components: {
+    RankingList
+  },
+  data() {
+    return {
+      scores: undefined
+    }
+  },
+  mounted() {
+    this.getScores()
+  },
+  methods: {
+    getScores() {
+      this.scores = [
+        { troop: 'Toop 1', points: 1 },
+        { troop: 'Troop 2', points: 2 }
+      ]
+    }
+  }
+}
+</script>

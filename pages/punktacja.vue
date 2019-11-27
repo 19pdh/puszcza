e80cf987c1655024a3c6e9f175ae691a0f6fe5cc
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-11-28 00:42:58 +0100

    Add public key

--- pages/punktacja.vue
+++ pages/punktacja.vue
@@ -9,6 +9,10 @@
 <script>
 import RankingList from '~/components/Ranking/RankingList'
 
+const FAUNA_KEY = 'fnADeP_U0uACC4Hruw9JvjexNsvB-V-QjI3wr8yH'
+const b64encodedSecret = Buffer.from(FAUNA_KEY + ':').toString('base64')
+const FAUNA_REF = '250230903625744898'
+
 export default {
   components: {
     RankingList

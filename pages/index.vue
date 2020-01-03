753743cb06c1fd0a6d1a25bbdf2dd0207d04d6dc
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-01-03 18:41:55 +0100

    Add oboz site

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
       <h1>Ostatnie wpisy z kroniki</h1>
       <post-list :posts="posts" :max="4" />
     </div>
+    <oboz-widget />
     <facebook-feed />
   </div>
 </template>
@@ -13,11 +14,12 @@
 import JoinUs from '~/components/JoinUs.vue'
 import FacebookFeed from '~/components/Facebook/FacebookFeed.vue'
 import PostList from '~/components/Posts/PostList'
+import ObozWidget from '~/components/ObozWidget'
 import postListParentMixin from '~/components/Posts/PostList/parentMixin'
 
 export default {
   name: 'HomeView',
-  components: { JoinUs, FacebookFeed, PostList },
+  components: { JoinUs, FacebookFeed, PostList, ObozWidget },
   mixins: [postListParentMixin]
 }
 </script>
---
+++ pages/oboz/2020.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="oboz">
+    <h1>Obóz 2020</h1>
+    <p>W tym roku obóz odbędzie się nad jeziorem Sporym</p>
+    <div class="mapouter">
+      <div class="gmap_canvas">
+        <iframe
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?q=Jezioro%20Spore&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.oboz {
+  margin-top: 40px;
+}
+
+h1 {
+  font-size: 2em;
+}
+
+.mapouter,
+.gmap_canvas,
+#gmap_canvas {
+  width: 100%;
+  height: 50vh;
+  max-height: 300px;
+}
+</style>

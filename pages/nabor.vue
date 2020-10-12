bd2f3c02c0f3987c09c2e02091e3b4831b9d6a29
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-10-12 21:12:50 +0200

    Form width

--- pages/nabor.vue
+++ pages/nabor.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
 <h2>Nabór 2020</h2>
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdBY-WAwsy13LQIoNl9MGYzBFZbTx8ynGf19oMyCMwurj1g8Q/viewform?embedded=true" width="640" height="1122" frameborder="0" marginheight="0" marginwidth="0">Ładuję…</iframe>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdBY-WAwsy13LQIoNl9MGYzBFZbTx8ynGf19oMyCMwurj1g8Q/viewform?embedded=true" width="100%" height="1122" frameborder="0" marginheight="0" marginwidth="0">Ładuję…</iframe>
 </div>
 </template>

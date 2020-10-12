7e67bf60672c21b40933f278f8d10be8c576daa7
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-10-12 21:04:15 +0200

    Add nabor

---
+++ pages/nabor.vue
@@ -0,0 +1,6 @@
+<template>
+<div>
+<h2>Nabór 2020</h2>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdBY-WAwsy13LQIoNl9MGYzBFZbTx8ynGf19oMyCMwurj1g8Q/viewform?embedded=true" width="640" height="1122" frameborder="0" marginheight="0" marginwidth="0">Ładuję…</iframe>
+</div>
+</template>
